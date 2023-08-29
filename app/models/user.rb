1c91e4b546854e9a9f10c43d0da4f3ea62905ef3
Zummerdev <140972912+Zummerdev@users.noreply.github.com>
2023-08-29 17:53:27 +0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
   has_many :projects
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         def manager?
-          user_type == 'manager'
-        end
+  def manager?
+    user_type == 'manager'
+  end
 end
+
